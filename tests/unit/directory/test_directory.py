0e589a3e8a16e8f7bb1f2114acbd6bcc488b31a2
Joshua Goring <sizzlingvortex@gmail.com>
2016-08-28 08:58:44 -0500

    Add test file for the directory package

---
+++ tests/unit/directory/test_directory.py
@@ -0,0 +1,11 @@
+"""Contains the unit tests for the inner directory package"""
+
+import unittest
+
+
+class TestDirectory(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
