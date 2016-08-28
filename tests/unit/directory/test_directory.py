e47b7e5952d4001459aee5ba570a7cc6d4c10d43
Joshua Goring <sizzlingvortex@gmail.com>
2016-08-28 09:35:38 -0500

    Add import of the InvalidDirectoryValueError to the directory package's test file

--- tests/unit/directory/test_directory.py
+++ tests/unit/directory/test_directory.py
@@ -3,7 +3,7 @@
 import unittest
 import os
 
-from classyfd import Directory
+from classyfd import Directory, InvalidDirectoryValueError
 
 class TestDirectory(unittest.TestCase):
     def setUp(self):
@@ -14,6 +14,8 @@ class TestDirectory(unittest.TestCase):
         d = Directory(self.fake_path)
         self.assertTrue(d)
         return
+    
+  
 
 
 if __name__ == "__main__":
