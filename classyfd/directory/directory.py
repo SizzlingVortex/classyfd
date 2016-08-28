adc89b882d56b86d2d56effc2fd22ec951ad892d
Joshua Goring <sizzlingvortex@gmail.com>
2016-08-28 09:14:45 -0500

    Add the Directory class to classyfd/directory/directory.py

--- classyfd/directory/directory.py
+++ classyfd/directory/directory.py
@@ -1,1 +1,9 @@
 """Contains a Directory class to represent real directories"""
+
+from ..base import _BaseFileAndDirectoryInterface
+
+
+class Directory(_BaseFileAndDirectoryInterface):
+    """A class that groups together the (meta)data and behavior of 
+    directories"""
+    pass
